6a9d02412d48b4229299e372d98ff018363b46e6
Gravatinha <gleberphant@gmail.com>
2026-03-28 12:24:41 +0000

    arvore

--- src/Infraestrutura/EstruturasDeDados/Arvores/ArvoreBinaria.java
+++ src/Infraestrutura/EstruturasDeDados/Arvores/ArvoreBinaria.java
@@ -1,6 +1,42 @@
 package Infraestrutura.EstruturasDeDados.Arvores;
+class NoArvore {
 
-import Nos.NoArvore;
+    int data;
+    NoArvore nextEsquerdo;
+    NoArvore nextDireito;
+
+    public NoArvore(int value){
+        data= value;
+    }
+
+
+    public int GetData() {
+        return data;
+    }
+
+
+    public NoArvore GetDireito(){
+        return nextDireito;
+    }
+
+
+    public NoArvore GetEsquerdo(){
+        return nextEsquerdo;
+    }
+
+
+    public void SetDireito(NoArvore newNode){
+        nextDireito = newNode;
+    }
+
+
+    public void SetEsquerdo(NoArvore newNode){
+        nextEsquerdo = newNode;
+    }
+
+}
+
+
 
 public class ArvoreBinaria {
     NoArvore head;
